0d9f5edf2d554cf9671a6839899fc49633fac27b
sachin.kumar <sachin.kumar.maheshwarappa@gmail.com>
2024-02-21 08:41:42 +0530

    Prefix Sum

--- Prefix SUm/prefixSum.py
+++ Prefix SUm/prefixSum.py
@@ -18,6 +18,9 @@ def prefixArray(arr):
 arr = [5,10,1,2,6]
 print(prefixArray(arr))
 
+#time complexity : O(n), since we are iterating only once over the given array
+#space complexity : O(n), since we are creating a new array of same length of the given array.  
+
 
 
 
