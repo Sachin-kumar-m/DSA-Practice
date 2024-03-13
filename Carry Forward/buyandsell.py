47045e2f5791d4297229e247c0b64b2e9cc2a88a
sachin.kumar <sachin.kumar.maheshwarappa@gmail.com>
2024-03-13 22:17:22 +0530

    max profit from buy and sell stock

---
+++ Carry Forward/buyandsell.py
@@ -0,0 +1,26 @@
+'''
+Say you have an array, A, for which the ith element is the price of a given stock on day i.
+If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+Return the maximum possible profit.
+'''
+
+A = [2,1,3,4,5,1,2,7,8,4,5]
+
+def stock(A):
+    minPrice = A[0]
+    maxProfit = 0
+    for i in range(1,len(A)):
+        if A[i]<minPrice:
+            minPrice = A[i]
+            minDay = i
+        else:
+            maxProfit = max(maxProfit,A[i]-minPrice)
+            
+
+    return maxProfit
+
+print(stock(A))
+
+#Time Complexity : O(N)
+#Space Complexity : O(1)
