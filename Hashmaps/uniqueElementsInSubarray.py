3abfa16a6dff79acb26444f16bb8cabae3440d73
Sachin Kumar <83578510+Sachin-kumar-m@users.noreply.github.com>
2024-09-08 14:58:35 +0530

    Sachin (#35)
    * find unique elements in a subaaray of size k
    
    * added time and space complexity

---
+++ Hashmaps/uniqueElementsInSubarray.py
@@ -0,0 +1,51 @@
+'''
+Given an integer array, we need to find unique elements in the subarray ok size k
+'''
+
+'''
+Approach: We can use the sliding window technique and also use hashmap to store the values of the subarray
+
+when moving our window, we can check if the new element is present in our hashmap, if it is, we increase the 
+frequency, if not we add it to our hashmap, 
+we also check the exit element, if the frequency of the exit element is 1, then we remove that element entirely
+if not we decrese the frequency value,
+and then the length of the hashmap is our answer
+we do this for every subarray of size k
+'''
+
+A = [2,4,3,8,3,9,4,9,4,10]
+
+k = 4
+
+def uniqueElementsInSubarray(A,k):
+    frequencyMap = {}
+    #first we iterate thourgh the first window and create our frequency map
+
+    for i in range(k):
+        if A[i] not in frequencyMap:
+            frequencyMap[A[i]] = 1
+        elif A[i] in frequencyMap:
+            frequencyMap[A[i]]+=1
+    print(len(frequencyMap))
+    s = 1
+    e = k
+    while e<len(A):
+        #checking the exit element, if the freq in more then 1 decrease the freq if not remove it entirely
+        if frequencyMap[A[s-1]]==1:
+            del frequencyMap[A[s-1]]
+        else:
+            frequencyMap[A[s-1]]-=1
+        #now i want to check for the incoming element
+        if A[e] in frequencyMap:
+            frequencyMap[A[e]]+=1 #if the element is already present increase the frequency value, else add it to the hasmap
+        else:
+            frequencyMap[A[e]]=1
+        print(len(frequencyMap))
+        s+=1
+        e+=1
+
+
+uniqueElementsInSubarray(A,k)
+
+# TC : O(N) + O(K) = O(N), we are iterating through the array only once
+# SC : O(N), worst case scenario, if all the elemts in the array are unique and k = len(A), then we will be storing N elements in our hashmap
